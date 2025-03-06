fbb47937f4a5440264a79eecf1bb52707d8e9d2e
Sérgio Dantas <sergiodnts828@gmail.com>
2025-03-05 21:39:41 -0300

    build: configure biome

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-import express from "express";
-import path from "path";
-import "dotenv/config";
+import path from "node:path"
+import express from "express"
+import "dotenv/config"
 
-const app = express();
-const port = process.env.PORT;
+const app = express()
+const port = process.env.PORT
 
-app.use(express.static(path.join(import.meta.dirname, "public")));
+app.use(express.static(path.join(import.meta.dirname, "public")))
 
-app.set("views", path.join(import.meta.dirname, "views"));
-app.set("view engine", "ejs");
+app.set("views", path.join(import.meta.dirname, "views"))
+app.set("view engine", "ejs")
 
 app.get("/", (req, res) => {
-  res.render("index");
-});
+  res.render("index")
+})
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+  console.log(`Server running on http://localhost:${port}`)
+})
